f83c86a11ef44099973469e5dc3f791ae50609ca
andrxpie <venganpro12@gmail.com>
2024-09-26 16:44:37 +0300

    №6

---
+++ №6/№6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Ball {
+    std::string m_color{ "Red" };
+    double m_radius{ 20. };
+
+public:
+    Ball(double radius) {
+        m_radius = radius;
+    }
+
+    Ball(std::string color = "Red", double radius = 20.) {
+        m_color = color;
+        m_radius = radius;
+    }
+
+    void print() {
+        std::cout << "color: " << m_color << ", radius: " << m_radius << "\n";
+    }
+};
+
+int main()
+{
+    Ball def;
+    def.print();
+
+    Ball black("Black");
+    black.print();
+
+    Ball thirty(30.);
+    thirty.print();
+
+    Ball blackThirty("Black", 30.);
+    blackThirty.print();
+}
