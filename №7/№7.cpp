4d4c618e8e6e0c2ded59ce623de871668c84fcae
andrxpie <venganpro12@gmail.com>
2024-10-23 18:50:38 +0300

    Deleting duplicate.

--- №7/№7.cpp
+++ №7/№7.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include <cstdint>
 
-class RGBA {
+class RGBA 
+{
     uint8_t m_red,
         m_green,
         m_blue,
@@ -9,9 +10,12 @@ class RGBA {
 
 public:
     RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255)
-        : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha) {}
+        : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha) 
+    {
+    }
 
-    void print() {
+    void print() 
+    {
         std::cout << "r=" << +m_red << " g=" << +m_green << " b=" << +m_blue << " a=" << +m_alpha << "\n";
     }
 };
